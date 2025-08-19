396e7dc46229c6d431ccfcfbc632bbb071c88b78
paulineSWE <pauline.onier@holbertonschool.com>
2025-08-19 11:07:30 +0200

    task

---
+++ heap_insert/binary_trees.h
@@ -0,0 +1,29 @@
+#ifndef _BINARY_TREES_H_
+#define _BINARY_TREES_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct binary_tree_s - Binary tree node
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
+typedef struct binary_tree_s
+{
+    int n;
+    struct binary_tree_s *parent;
+    struct binary_tree_s *left;
+    struct binary_tree_s *right;
+} binary_tree_t;
+
+/* Prototype of the function to create a node */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+
+/* Provided print function */
+void binary_tree_print(const binary_tree_t *tree);
+
+#endif /* _BINARY_TREES_H_ */
